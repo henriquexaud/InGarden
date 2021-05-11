0cf40728ffb34ef8f127e0bf52125e56904fc979
Henrique Xaud <xxhenriquexaud@gmail.com>
2021-05-11 14:59:01 -0400

    landing page finished

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
+import wateringImg from '../assets/watering.png';
+import { Button } from '../components/Button';
 import colors from '../styles/colors';
 
 export function Welcome() {
+
     return (
         <SafeAreaView style={styles.container}>
+
             <Text style={styles.title}>
                 Gerencie {'\n'}
-                sua horta de {'\n'}
-                forma fácil
+                suas plantas {'\n'}
+                de forma fácill
             </Text>
+
+            <Image source={wateringImg} />
+
+            <Button title="Começar" />
+
         </SafeAreaView>
     )
 }
@@ -19,11 +28,16 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'center'
     },
     title: {
         fontSize: 32,
         textAlign: 'center',
         color: colors.heading,
+        marginBottom: 50
+    },
+    image: {
+        width: 292,
+        height: 284
     }
 })
