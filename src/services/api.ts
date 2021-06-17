d107d1cf9eba1bf5e4ea0124a86c173f25ad2e2c
Henrique Xaud <xxhenriquexaud@gmail.com>
2021-06-17 06:17:40 -0400

    fixing database

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://10.0.0.170:3333', //psj
+    baseURL: 'http://192.168.1.100:3333' //casa
 });
 
 export default api;
 
-// to active api: json-server ./src/services/server.json --host 192.168.1.21 --port 3333 --delay 500
+// to active api: json-server ./src/services/server.json --host 192.168.1.100 --port 3333 --delay 800
